7b75f8d05e802d2dc06c9358f0cf740225d9894f
Jhoncito07 <jhonjairocontrerasegido@gmail.com>
2024-05-18 17:41:26 -0500

    Versión Inicial

---
+++ libros/src/main/java/com/demo/libros/repositories/LibroRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.demo.libros.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.demo.libros.models.Libro;
+
+@Repository
+public class LibroRepositoryImpl implements LibroRepository {
+    private final List<Libro> libros = new ArrayList<>();
+    public LibroRepositoryImpl() {
+        libros.add(new Libro(1, "El señor de los anillos", "J.R.R. Tolkien", "Minotauro", 1954));
+        libros.add(new Libro(2, "Harry Potter y la piedra filosofal", "J.K. Rowling", "Salamandra", 1997));
+        libros.add(new Libro(3, "Orgullo y prejuicio", "Jane Austen", "Penguin Clásicos", 1813));
+        libros.add(new Libro(4, "Crónica de una muerte anunciada", "Gabriel García Márquez", "Sudamericana", 1981));
+        libros.add(new Libro(5, "Las aventuras de Sherlock Holmes", "Arthur Conan Doyle", "Alba Editorial", 1892));
+    }
+
+    @Override
+    public Libro getLibro(int id) {
+        System.out.println("Estableciendo conexión con la base de datos Oracle.");
+        for (Libro libro : libros) {
+            if (id == libro.getId())
+                return libro;
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<Libro> getLibros() {
+        System.out.println("Estableciendo conexión con la base de datos Oracle.");
+        return libros;
+    }
+
+    @Override
+    public Libro nuevLibro(Libro libro) {
+        libros.add(libro);
+        return libro;
+    }
+
+}
